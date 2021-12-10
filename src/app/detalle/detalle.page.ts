b3ad2784f122c65074dbbf168f9dee5c1047cdac
adriansoriagarcia <adri_polixero@hotmail.com>
2021-12-10 11:15:04 +0100

    arreglo detalle

--- src/app/detalle/detalle.page.ts
+++ src/app/detalle/detalle.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute } from '@angular/router';
 import { Reparacion } from '../reparacion';
+import { FirestoreService } from '../firestore.service';
 
 @Component({
   selector: 'app-detalle',
@@ -17,20 +18,22 @@ export class DetallePage implements OnInit {
     data: {} as Reparacion
   };
 
-  this.firestoreService.consultarPorId("reparaciones", id).subscribe((resultado) => {
-    // Preguntar si se hay encontrado un document con ese ID
-    if(resultado.payload.data() != null) {
-      this.document.id = resultado.payload.id
-      this.document.data = resultado.payload.data();
-      // Como ejemplo, mostrar el nombre del cliente en consola
-      console.log(this.document.data.nombre);
-    } else {
-      // No se ha encontrado un document con ese ID. Vaciar los datos que hubiera
-      this.document.data = {} as Reparacion;
-    } 
-  });
+  
 
-  constructor(private activatedRoute: ActivatedRoute) { };
+  constructor(private activatedRoute: ActivatedRoute, private firestoreService: FirestoreService) {
+    this.firestoreService.consultarPorId("reparaciones", this.id).subscribe((resultado) => {
+      // Preguntar si se hay encontrado un document con ese ID
+      if(resultado.payload.data() != null) {
+        this.document.id = resultado.payload.id
+        this.document.data = resultado.payload.data();
+        // Como ejemplo, mostrar el nombre del cliente en consola
+        console.log(this.document.data.nombre);
+      } else {
+        // No se ha encontrado un document con ese ID. Vaciar los datos que hubiera
+        this.document.data = {} as Reparacion;
+      } 
+    });
+   };
 
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.paramMap.get('id')
