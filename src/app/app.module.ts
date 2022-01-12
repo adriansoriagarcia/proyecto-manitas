2d819a8af62dedbe1bfa5adf3ebda099702b0932
kaliuser44 <56774233+kaliuser44@users.noreply.github.com>
2022-01-12 20:48:23 +0100

    comienzo fase 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,17 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
 
+//import { ImagePicker } from '@awesome-cordova-plugins/image-picker/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    //ImagePicker,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
